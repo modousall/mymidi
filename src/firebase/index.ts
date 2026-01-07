3101c4ed1a4cd4c8ece0ece0606ef341c2000d5b
Modou Sall <modousall1@gmail.com>
2026-01-07 12:20:56 +0000

    continuer ces changement

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,7 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { Auth, getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
@@ -48,3 +48,6 @@ export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
+
+// Re-export auth methods for convenience
+export { signInWithEmailAndPassword, createUserWithEmailAndPassword };
